a877578cc61acc2a6af2f5b9e7b34cd59bf89546
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-02-17 20:52:38 -0300

    feat: GET Customers. Endpoint to get customers details
    This endpoint will return the customers details as a list of customers.

--- src/controllers/v0/customers/routes/customer.router.ts
+++ src/controllers/v0/customers/routes/customer.router.ts
@@ -3,6 +3,9 @@ import { Customer } from '../models/Customer'
 
 export const router: Router = Router();
 
+const HTTP_OK = 200;
+const HTTP_CREATED = 201;
+
 router.post('/', async (request: Request, response: Response) => {
 
     const newCustomer = await new Customer({
@@ -12,7 +15,13 @@ router.post('/', async (request: Request, response: Response) => {
 
     const newSavedCustomer = await newCustomer.save();
 
-    response.status(201).send(newSavedCustomer);
+    response.status(HTTP_CREATED).send(newSavedCustomer);
+});
+
+
+router.get('/', async (request: Request, response: Response) => {
+    const customers = await Customer.findAndCountAll({order: [['email', 'DESC']]})
+    response.status(HTTP_OK).send(customers);
 });
 
 export const CustomerRouter: Router = router;
